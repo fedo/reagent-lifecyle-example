dc6ddafd9a3e92855894b15d27d1eafe8f3246ac
Federico Roffi <fedoverseas@gmail.com>
2016-03-08 17:06:32 +0000

    Add Verbose Component

--- src/reagent_lifecyle_example/core.cljs
+++ src/reagent_lifecyle_example/core.cljs
@@ -7,10 +7,44 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:text "Hello world!"
+                          :counter1 0
+                          :counter2 0}))
+
+(defn verbose-component
+  [label n]
+  (println "[verbose-component] fn call")
+  (reagent/create-class
+    {:component-did-mount
+     #(println "[verbose-component] component-did-mount (label=" label ")" )
+
+     :component-will-mount
+     #(println "[verbose-component] component-will-mount (label=" label ")")
+
+     :reagent-render ;; Note:  is not :render
+     (fn [label n] ;; remember to repeat parameters
+       (println "[verbose-component] reagent-render (label=" label ")")
+       [:div
+        [:h3 "Verbose Component (" label ")"]
+        [:div "n = " (str n)]])}))
 
 (defn hello-world []
-  [:h1 (:text @app-state)])
+  [:div
+   [:h1 "reagent-lifecyle-example"]
+   [:div "@app-state =" (str @app-state)]
+
+   (into [:div] (map (fn [k]
+                       [:button {:on-click (fn [e]
+                                             (println "inc " (str k))
+                                             (swap! app-state assoc k (inc (get @app-state k))))} (str "inc " k)])
+                     [:counter1 :counter2]))
+
+   [:div
+    [:h2 "Verbose Component 1"]
+    [verbose-component "one" (@app-state :counter1)]]
+   [:div
+    [:h2 "Verbose Component 2"]
+    [verbose-component "two" (@app-state :counter2)]]])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
@@ -20,4 +54,4 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
